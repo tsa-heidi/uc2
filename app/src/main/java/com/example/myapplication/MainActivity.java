fa3a6b30cb189de40685bfe739d517db309d8e36
cookiemonsterleni <epartakki@smith.edu>
2020-01-14 15:30:41 -0500

    Merge branch 'master' of https://github.com/tsa-heidi/uc2
    # Conflicts:
    #	app/src/main/java/com/example/myapplication/MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,9 +22,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int arr[] = {10,20,30,40,50};
-        int target = 30;
-        int last=arr.length-1;
 
 
         input_to_id.put("km", "length");
@@ -58,41 +55,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         outputNumber.setText("");
     }
 
-    public static void binarySearch(int arr[], int first, int last, int target){
-        int mid = (first + last)/2;
-        while (first <= last){
-            if (arr[mid] < target){
-                first = mid + 1;
-            } else if (arr[mid] == target){
-                System.out.println("Function" +
-                        " found at index: " + mid);
-                break;
-            } else{
-                last = mid - 1;
-            }
-            mid = (first + last)/2;
-        }
-        if (first > last){
-            System.out.println("Function not found!");
-        }
-        binarySearch(arr,0,last,target);
-
-    }
-
-    public static int linearSearch(int arr[], int target){
-        int result = linearSearch(arr, target);
-        if(result == -1)
-            System.out.print("Function not present!");
-        else
-            System.out.print("Function is present!" + result);
-
-
-        int n = arr.length;
-            for(int i = 0; i < n; i++){
-                if(arr[i] == target)
-                    return i;
-            }
-            return -1;
-        }
 
 }
